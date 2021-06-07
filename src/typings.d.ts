5f737c51b0ce6b5c793647b846d05d8a753648c3
naimdanialjohar <37859609+naimdanialjohar@users.noreply.github.com>
2021-06-08 01:00:17 +0800

    added new packages, new configurations

---
+++ src/typings.d.ts
@@ -0,0 +1,27 @@
+declare module "*.modules.css" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.scss" {
+  interface IClassNames {
+    [className: string]: string;
+  }
+  const classNames: IClassNames;
+  export = classNames;
+}
+
+declare module "*.module.sass" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.less" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module "*.module.styl" {
+  const classes: { [key: string]: string };
+  export default classes;
+}
